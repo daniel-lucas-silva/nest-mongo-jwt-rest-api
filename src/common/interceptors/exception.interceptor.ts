e37fc57c0978b31d9babaa3e7fa09999ab0e5104
daniel-lucas-silva <dluc18@gmail.com>
2019-04-21 16:48:58 -0300

    creating commom module

---
+++ src/common/interceptors/exception.interceptor.ts
@@ -0,0 +1,21 @@
+import { NestInterceptor, Injectable, CallHandler, ExecutionContext, HttpException, HttpStatus } from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ExceptionInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next
+      .handle()
+      .pipe(
+        catchError(err =>
+          throwError(
+            new HttpException(
+              'Exception interceptor message',
+              HttpStatus.BAD_GATEWAY,
+            ),
+          ),
+        ),
+      );
+  }
+}
---
+++ src/common/pipes/validation.pipe.ts
@@ -0,0 +1,27 @@
+import { Injectable, PipeTransform, ArgumentMetadata, BadRequestException, Type } from '@nestjs/common';
+import { validate } from 'class-validator';
+import { plainToClass } from 'class-transformer';
+
+@Injectable()
+export class ValidationPipe implements PipeTransform<any> {
+  async transform(value: any, metadata: ArgumentMetadata) {
+    const { metatype } = metadata;
+
+    if (!metatype || !this._toValidate(metatype)) {
+      return value;
+    }
+
+    const object = plainToClass(metatype, value);
+    const errors = await validate(object);
+
+    if (errors.length > 0) {
+      throw new BadRequestException('Validation failed');
+    }
+    return value;
+  }
+
+  private _toValidate(metatype: Type<any>): boolean {
+    const types = [String, Boolean, Number, Array, Object];
+    return !types.find(type => metatype === type);
+  }
+}
