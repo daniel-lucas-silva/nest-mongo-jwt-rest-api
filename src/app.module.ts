0445617e8844d40d955c5a9dcf5cb136ad9a12f7
daniel-lucas-silva <dluc18@gmail.com>
2019-04-21 16:51:10 -0300

    generating post module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
-
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { PostModule } from './post/post.module';
+
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     UserModule,
     AuthModule,
+    PostModule,
   ],
 })
 export class AppModule {
