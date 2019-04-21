b097e21e7271a85a4cc059ccd8e2ac53d5e1dd47
daniel-lucas-silva <dluc18@gmail.com>
2019-04-20 22:31:56 -0300

    add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { CityModule } from './city/city.module';
 
 @Module({
-
   imports: [
+    TypeOrmModule.forRoot(),
     UserModule,
     AuthModule,
     CityModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly connection: Connection) {}
+}
