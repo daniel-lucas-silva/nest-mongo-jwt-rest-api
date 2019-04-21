de9f3519ceb292c5a9e742b28caa4b880f1e7aa9
daniel-lucas-silva <dluc18@gmail.com>
2019-04-21 16:50:55 -0300

    removing city module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,12 @@ import { Connection } from 'typeorm';
 
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { CityModule } from './city/city.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     UserModule,
     AuthModule,
-    CityModule,
   ],
 })
 export class AppModule {
