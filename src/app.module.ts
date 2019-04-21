0108ce39426cac7179481c2bc689383a7f6c446f
daniel-lucas-silva <dluc18@gmail.com>
2019-04-21 16:57:08 -0300

    testing middlware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PostModule } from './post/post.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 
 @Module({
@@ -14,6 +15,12 @@ import { PostModule } from './post/post.module';
     PostModule,
   ],
 })
-export class AppModule {
+export class AppModule implements NestModule {
   constructor(private readonly connection: Connection) {}
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'users', method: RequestMethod.GET });
+  }
 }
