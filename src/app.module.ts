7236996eb6d4e779cec037731588cdc1b1144640
daniel-lucas-silva <dluc18@gmail.com>
2019-04-20 22:12:34 -0300

    base scaffold

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,14 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { CityModule } from './city/city.module';
 
 @Module({
+
+  imports: [
+    UserModule,
+    AuthModule,
+    CityModule,
+  ],
 })
 export class AppModule {}
