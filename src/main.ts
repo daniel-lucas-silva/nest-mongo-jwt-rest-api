aac2d5462940f9dfe75afa74eeda4d19e9aa2be8
daniel-lucas-silva <dluc18@gmail.com>
2019-04-20 21:52:41 -0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import {
+  DocumentBuilder,
+  SwaggerModule,
+} from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.setGlobalPrefix('api');
+
+  if (process.env.NODE_ENV === 'development') {
+    const options = new DocumentBuilder()
+    .setTitle('Fazendo Mais App')
+    .setDescription('The Fazendo Mais API description')
+    .setVersion('1.0')
+    .setBasePath('api')
+    .addBearerAuth()
+    .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('/docs', app, document);
+
+  }
+
+  await app.listen(3000);
+}
+bootstrap();
